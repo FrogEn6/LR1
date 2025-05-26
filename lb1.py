4bc21f65992445edfb0a107dbdf7f29145f3ecd3
Протопопов Артем <153822459+FrogEn6@users.noreply.github.com>
2025-05-26 21:04:51 +0000

    generate.py

--- lb1.py
+++ lb1.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 import random
 import sys
-sys.stderr=open('errors.txt','a')
-A=random.randint(-10,10)
-print(A)
-sys.stderr=open('errors.txt','w')
+
+def main():
+    try:
+        A = random.randint(-10,10)
+        print(A)
+        with open("logs.txt", "a") as log_file:
+            log_file.write(f"A={A}\n")
+    except Exception as e:
+        print(f"Error: {str(e)}", file=sys.stderr)
+
+if __name__ =="__main__":
+    main()
