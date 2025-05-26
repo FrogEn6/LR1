576e2afda213e3b0a6a896a75dd247cdd1de37de
Протопопов Артем <153822459+FrogEn6@users.noreply.github.com>
2025-05-26 21:06:27 +0000

    Calc_sqrt.py

--- lb3.py
+++ lb3.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python3
 import sys
-sys.stderr=open('errors.txt','a')
-A=int(input("Write number: "))
-A=A**0.5
-print("sqrt(A)=", A)
-res=open("output.txt",'w')
-res.write(str(A))
-res.close()
-sys.stderr=open('errors.txt','w')
+import math
+
+def main():
+    try:
+        C = float(sys.stdin.read().strip())
+        if C < 0:
+            raise ValueError("It is impossible to find the root of a negative number")
+        result = math.sqrt(C)
+
+        with open("output.txt", "w") as output_file:
+            output_file.write(f"Square root of {C} is {result}\n")
+        with open("logs.txt", "a") as log_file:
+            log_file.write(f"C = {C}, sqrt(C) = {result}\n")
+
+    except ValueError as e:
+        sys.exit(1)
+    except Exception as e:
+        print(f"Error: {str(e)}", file=sys.stderr)
+        sys.exit(1)
+
+if __name__ == "__main__":
+    main()
